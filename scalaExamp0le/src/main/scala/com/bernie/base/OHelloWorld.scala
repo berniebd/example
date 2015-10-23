e3e8db1486b25affb457e889374e6310bbad74bc
bida <berniebd05@gmail.com>
2015-10-23 18:35:03 +0800

    update

--- scalaExamp0le/src/main/scala/com/bernie/base/OHelloWorld.scala
+++ scalaExamp0le/src/main/scala/com/bernie/base/OHelloWorld.scala
@@ -3,8 +3,13 @@ package com.bernie.base
 /**
  * Created by bida on 2015/10/22.
  */
-class OHelloWorld {
-  def main(args: Array[String]): Unit ={
-    println("hello world object")
-  }
+class Rational(n:Int, d:Int){
+  require(d!=0)
+  val number = n
+  val denom = d
+  override def toString = number + "/" + denom
+  def add(that:Rational) =
+    new Rational(
+      number * that.denom + that.number* denom, denom * that.denom
+    )
 }
